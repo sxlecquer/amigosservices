7cd2fb68fbafe9c75333c9cda30a93c462696020
Yan Samokar <ysamokar228@gmail.com>
2024-02-18 22:08:56 +0100

    Add PostgreSQL connection
    Created customers are saved to the database by sending a POST request to '/api/v1/customers'.

--- customer/src/main/java/com/example/customer/entity/Customer.java
+++ customer/src/main/java/com/example/customer/entity/Customer.java
@@ -1,11 +1,27 @@
 package com.example.customer.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class Customer {
+    @Id
+    @SequenceGenerator(
+            name = "CUSTOMER_SEQ",
+            sequenceName = "customer_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "CUSTOMER_SEQ"
+    )
     private Integer id;
     private String firstName;
     private String lastName;
--- customer/src/main/java/com/example/customer/service/CustomerService.java
+++ customer/src/main/java/com/example/customer/service/CustomerService.java
@@ -2,17 +2,18 @@ package com.example.customer.service;
 
 import com.example.customer.entity.Customer;
 import com.example.customer.model.CustomerRegistrationRequest;
+import com.example.customer.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
-
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
                 .lastName(request.lastName())
                 .email(request.email())
                 .build();
-        // todo: validate email and save customer to db
+        // todo: validate email
+        customerRepository.save(customer);
     }
 }
