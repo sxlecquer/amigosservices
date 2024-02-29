33c434cc4f22fc7680193a45daa71c9fe3d93535
Yan Samokar <ysamokar228@gmail.com>
2024-02-29 22:28:59 +0100

    Fix kafka service in 'docker-compose.yml'
    Now Kafka consumer ('notification') receives messages produced by 'customer'.

--- notification/src/main/java/com/example/notification/NotificationApplication.java
+++ notification/src/main/java/com/example/notification/NotificationApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(scanBasePackages = {"com.example.kafka", "com.example.notification"})
+@SpringBootApplication(scanBasePackages = {"com.example.notification", "com.example.kafka"})
 @EnableDiscoveryClient
 public class NotificationApplication {
 
